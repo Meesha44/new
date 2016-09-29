e5c394dfbfb6e5f486af3fd7866051f5255fbdb8
Meesha <meesha.kmr@gmail.com>
2016-09-29 19:34:34 +0530

    sds

--- jeMaven/src/test/java/jenk/secondlassTest.java
+++ jeMaven/src/test/java/jenk/secondlassTest.java
@@ -3,13 +3,10 @@ package jenk;
 import org.testng.annotations.Test;
 
 public class secondlassTest {
-  @Test
-  public void f() {
-	  System.out.println("Secondclass");
-  }
   
   @Test
   public void f11() {
 	  System.out.println("New method just for checkin3");
   }
+  
 }
